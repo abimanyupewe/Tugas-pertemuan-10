884b4dbbd35e253d386a522acdd2ed867d795095
abimanyupewe <abimanyupw369@gmail.com>
2024-06-15 12:22:12 +0700

    tugas

---
+++ tugas_pertemuan_10/src/main/java/tugas_pertemuan_10_tebak_angka/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_pertemuan_10_tebak_angka;
+import tugas_pertemuan_10_tebak_angka.Game;
+
+/**
+ *
+ * @author A-11
+ */
+public class Main {
+
+    public static void main(String[] args) {
+// Create a new game object
+        Game game = new Game(10);
+// Start the game
+        game.play();
+
+    }
+}
